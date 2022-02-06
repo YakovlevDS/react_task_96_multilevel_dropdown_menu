20fc5f5d4d7c5811c163f266d9c96e8692d08040
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-02-06 17:37:13 +0200

    feat:add dropdown menu multilevel

---
+++ src/menuItems.js
@@ -0,0 +1,60 @@
+export const menuItems = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Services",
+    submenu: [
+      {
+        title: "web design",
+      },
+      {
+        title: "web development",
+        submenu: [
+          {
+            title: "Frontend",
+            submenu: [
+              {
+                title: "JS",
+              },
+              {
+                title: "CSS",
+              },
+              {
+                title: "SCSS",
+              },
+              {
+                title: "HTML",
+              },
+            ],
+          },
+          {
+            title: "Backend",
+            submenu: [
+              {
+                title: "NodeJS",
+              },
+              {
+                title: "PHP",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: "SEO",
+      },
+    ],
+  },
+  {
+    title: "About",
+    submenu: [
+      {
+        title: "Who we are",
+      },
+      {
+        title: "Our values",
+      },
+    ],
+  },
+];
